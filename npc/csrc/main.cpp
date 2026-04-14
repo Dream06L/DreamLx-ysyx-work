2e0adddc7d0622ca63a5f7757d7314c80cb69178
2025007002-Liang xin <3308558081@qq.com>
2026-04-14 14:14:23 +0800

    没搞好,数电实验混进来了

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,31 @@
-#include <stdio.h>
+#include <Vtop.h>
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+Vtop* top = new Vtop;//指针
+
+void nvboard_bind_all_pins(Vtop* top);//必备
+
+void single_cycle(){
+    top->clk=0;top->eval();
+    top->clk = 1; top->eval();
+}
+
+void reset(int n) {
+  top->rst = 1;
+  while (n -- > 0) single_cycle();
+  top->rst = 0;
+}
+
+int main(){
+
+  nvboard_bind_all_pins(top);
+  nvboard_init(); 
+   
+  reset(10) ;//复位10周期
+  while (1)
+  {
+    nvboard_update();
+    single_cycle();
+  }
+   
 }
